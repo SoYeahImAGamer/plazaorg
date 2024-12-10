1c95db6f96590fad4fe861b43048fd0c68b3b947
RobinsPlaza <seacreeperz@gmail.com>
2024-12-10 15:32:16 -0600

    -replaced xitter with bluesky -removed shorts from latest video playlist -updated about

--- scripts/about.js
+++ scripts/about.js
@@ -1,1 +1,17 @@
-document.write("Hi I'm Robin<br><br>I like art and programming (as you can tell by the website)<br>I've been making fun little edits and streaming for pretty much as long as I've had internet access<br>I also have a very serious addiction to Minecraft please send help");
+function age(dt2, dt1)
+{
+  // Calculate the difference in milliseconds between the two dates
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  // Convert the difference from milliseconds to days
+  diff /= (60 * 60 * 24);
+  // Calculate the approximate number of years by dividing the difference in days by the average number of days in a year (365.25)
+  return Math.abs(Math.round(diff / 365.25));
+}
+
+// Create two Date objects representing different dates
+const dob = new Date("June 14, 2004");
+const now = new Date();
+
+document.write("Hi I'm Robin<br><br>I'm " + age(now, dob) + " years old, and I've been streaming and making videos for about as long as I've had internet access");
+document.write("<br>I usually have a few art/programming projects going at once (like this website)")
+document.write("<br><br>Be sure to check out the various links around the website to:<br>Social Media<br>Game Mods<br>Games<br><sup>and maybe some hidden things</sup>");
--- scripts/version.js
+++ scripts/version.js
@@ -1,1 +1,1 @@
-document.write("Plaza v1.2.3");
+document.write("<br>Plaza v1.2.4");
